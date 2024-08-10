f0b22477587503a4c44f12c11e779c2f8a62aaac
NguyenVanAnit <annit1910@gmail.com>
2024-08-10 11:38:00 +0700

    create banner

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,14 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage : {
+        'banner': 'url(/banner.jpg)'
+      },
+      colors : {
+        'rbga-50': 'rbga(0, 0, 0, 0.5)'
+      }
+    },
   },
   plugins: [],
 }
